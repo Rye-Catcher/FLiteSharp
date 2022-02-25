478dc8771584d3eb13f1aa0a0c42b777f0eec43d
SimoneMezzaro <simone.mezzaro@mail.polimi.it>
2022-02-25 21:04:26 +0800

    Add javadoc to UndefinedComponent

--- src/main/java/flitesharp/component/literal/UndefinedComponent.java
+++ src/main/java/flitesharp/component/literal/UndefinedComponent.java
@@ -1,5 +1,8 @@
 package flitesharp.component.literal;
 
+/**
+ * A component representing an undefined literal. The result of the corresponding program is the undefined value.
+ */
 public class UndefinedComponent extends LiteralComponent{
     /**
      * {@inheritDoc}
@@ -9,11 +12,19 @@ public class UndefinedComponent extends LiteralComponent{
         return toCompare instanceof UndefinedComponent; // To be revised once we have type checking
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The program result of an UndefinedComponent is the undefined value.</p>
+     */
     @Override
     public LiteralComponent evaluate() {
         return this;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getStringRepresentation() {
         return "[Undefined]";
