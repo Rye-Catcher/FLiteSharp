86267ff5012a0e2f535c8d562e9f5c843fc4b17a
SimoneMezzaro <simone.mezzaro@mail.polimi.it>
2022-03-23 00:11:11 +0800

    Add tests for units of measure

--- app/src/test/java/flitesharp/UnitOfMeasure/FLiteSharpUnitsOfMeasureCreatorVisitorTest.java
+++ app/src/test/java/flitesharp/UnitOfMeasure/FLiteSharpUnitsOfMeasureCreatorVisitorTest.java
@@ -1,6 +1,7 @@
-package flitesharp.type;
+package flitesharp.UnitOfMeasure;
 
 import flitesharp.FLiteSharp;
+import flitesharp.unitOfMeasure.FLiteSharpUnitsOfMeasureCreatorVisitor;
 import io.antlr.gen.FLiteSharpLexer;
 import io.antlr.gen.FLiteSharpParser;
 import org.antlr.v4.runtime.CharStream;
@@ -10,23 +11,27 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Objects;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-public class FLiteSharpTypesCreatorVisitorTest {
-    private FLiteSharpTypesCreatorVisitor visitor;
+public class FLiteSharpUnitsOfMeasureCreatorVisitorTest {
+    private FLiteSharpUnitsOfMeasureCreatorVisitor visitor;
 
     private ParseTree getTree(String input) {
         CharStream inputStream = CharStreams.fromString(input);
         FLiteSharpLexer lexer = new FLiteSharpLexer(inputStream);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         FLiteSharpParser parser = new FLiteSharpParser(tokens);
-        return parser.typeDeclaration();
+        return parser.unitFormula();
     }
 
-    private String getExpectedResult(String path) {
+    private String getStringFromFile(String path) {
         InputStream inputStream = Objects.requireNonNull(FLiteSharp.class.getResourceAsStream(path));
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
         try {
@@ -39,17 +44,18 @@ public class FLiteSharpTypesCreatorVisitorTest {
 
     @Before
     public void setUp() {
-        visitor = new FLiteSharpTypesCreatorVisitor();
+        visitor = new FLiteSharpUnitsOfMeasureCreatorVisitor();
     }
 
     @Test
     public void extensiveTesting() {
-        int testNumber = 4;
-        String path = "/type/test";
+        int testNumber = 5;
+        String path = "/unitOfMeasure/";
         for(int i=1; i<=testNumber; i++) {
-            String expected = getExpectedResult(path + i + ".txt");
-            String result = getTree(expected).accept(visitor).getStringRepresentation();
-            assertEquals(result, expected);
+            String input = getStringFromFile(path + "test" + i + ".txt");
+            String expectedOutput = getStringFromFile(path + "sol" + i + ".txt");
+            String result = getTree(input).accept(visitor).getStringRepresentation();
+            assertEquals(expectedOutput, result);
         }
     }
 }
---
+++ app/src/test/java/flitesharp/UnitOfMeasure/UnitOfMeasureTest.java
@@ -0,0 +1,151 @@
+package flitesharp.UnitOfMeasure;
+
+import flitesharp.unitOfMeasure.UnitOfMeasure;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class UnitOfMeasureTest {
+    private UnitOfMeasure initialUnit;
+
+    @Before
+    public void setUp() {
+        Map<String, Integer> formula = new HashMap<>();
+        formula.put("a", 1);
+        formula.put("b", 3);
+        formula.put("c", -2);
+        formula.put("d", 0);
+        initialUnit = new UnitOfMeasure(formula);
+    }
+
+    @Test
+    public void match_twoEmptyUnits_shouldReturnTrue() {
+        assertTrue(new UnitOfMeasure().match(new UnitOfMeasure()));
+    }
+
+    @Test
+    public void match_unitsWithDifferentSize_shouldReturnFalse() {
+        Map<String, Integer> formula = new HashMap<>();
+        formula.put("a", 1);
+        formula.put("b", 3);
+        UnitOfMeasure toCompare = new UnitOfMeasure(formula);
+        assertFalse(initialUnit.match(toCompare));
+    }
+
+    @Test
+    public void match_unitsWithDifferentNames_shouldReturnFalse() {
+        Map<String, Integer> formula = new HashMap<>();
+        formula.put("a", 1);
+        formula.put("b", 3);
+        formula.put("e", -2);
+        UnitOfMeasure toCompare = new UnitOfMeasure(formula);
+        assertFalse(initialUnit.match(toCompare));
+    }
+
+    @Test
+    public void match_identicalUnits_shouldReturnTrue() {
+        Map<String, Integer> formula = new HashMap<>();
+        formula.put("a", 1);
+        formula.put("b", 3);
+        formula.put("c", -2);
+        UnitOfMeasure toCompare = new UnitOfMeasure(formula);
+        assertTrue(initialUnit.match(toCompare));
+    }
+
+    @Test
+    public void multiply_noNewUnitMultiplier_shouldReturnUnitsProduct() {
+        Map<String, Integer> multiplierFormula = new HashMap<>();
+        multiplierFormula.put("a", 1);
+        multiplierFormula.put("b", -3);
+        UnitOfMeasure multiplier = new UnitOfMeasure(multiplierFormula);
+        UnitOfMeasure result = initialUnit.multiply(multiplier);
+        Map<String, Integer> expectedResultFormula = new HashMap<>();
+        expectedResultFormula.put("a", 2);
+        expectedResultFormula.put("c", -2);
+        UnitOfMeasure expectedResult = new UnitOfMeasure(expectedResultFormula);
+        assertTrue(result.match(expectedResult));
+    }
+
+    @Test
+    public void multiply_newUnitMultiplier_shouldReturnUnitsProduct() {
+        Map<String, Integer> multiplierFormula = new HashMap<>();
+        multiplierFormula.put("a", 1);
+        multiplierFormula.put("b", -3);
+        multiplierFormula.put("e", -1);
+        UnitOfMeasure multiplier = new UnitOfMeasure(multiplierFormula);
+        UnitOfMeasure result = initialUnit.multiply(multiplier);
+        Map<String, Integer> expectedResultFormula = new HashMap<>();
+        expectedResultFormula.put("a", 2);
+        expectedResultFormula.put("c", -2);
+        expectedResultFormula.put("e", -1);
+        UnitOfMeasure expectedResult = new UnitOfMeasure(expectedResultFormula);
+        assertTrue(result.match(expectedResult));
+    }
+
+    @Test
+    public void multiply_emptyMultiplier_shouldReturnInitialUnit() {
+        UnitOfMeasure multiplier = new UnitOfMeasure();
+        UnitOfMeasure result = initialUnit.multiply(multiplier);
+        assertTrue(result.match(initialUnit));
+    }
+
+    @Test
+    public void multiply_inverseMultiplier_shouldReturnEmptyUnit() {
+        Map<String, Integer> multiplierFormula = new HashMap<>();
+        multiplierFormula.put("a", -1);
+        multiplierFormula.put("b", -3);
+        multiplierFormula.put("c", 2);
+        UnitOfMeasure multiplier = new UnitOfMeasure(multiplierFormula);
+        UnitOfMeasure result = initialUnit.multiply(multiplier);
+        UnitOfMeasure expectedResult = new UnitOfMeasure();
+        assertTrue(result.match(expectedResult));
+    }
+
+    @Test
+    public void computeExponential_zeroExponent_shouldReturnEmptyUnit() {
+        UnitOfMeasure result = initialUnit.computeExponential(0);
+        assertTrue(result.match(new UnitOfMeasure()));
+    }
+
+    @Test
+    public void computeExponential_minusOneExponent_shouldReturnInverseUnit() {
+        UnitOfMeasure result = initialUnit.computeExponential(-1);
+        Map<String, Integer> expectedResultFormula = new HashMap<>();
+        expectedResultFormula.put("a", -1);
+        expectedResultFormula.put("b", -3);
+        expectedResultFormula.put("c", 2);
+        UnitOfMeasure expectedResult = new UnitOfMeasure(expectedResultFormula);
+        assertTrue(result.match(expectedResult));
+    }
+
+    @Test
+    public void computeExponential_exponent_shouldReturnUnitToTheExponent() {
+        UnitOfMeasure result = initialUnit.computeExponential(3);
+        Map<String, Integer> expectedResultFormula = new HashMap<>();
+        expectedResultFormula.put("a", 3);
+        expectedResultFormula.put("b", 9);
+        expectedResultFormula.put("c", -6);
+        UnitOfMeasure expectedResult = new UnitOfMeasure(expectedResultFormula);
+        assertTrue(result.match(expectedResult));
+    }
+
+    @Test
+    public void divide_divisor_shouldReturnQuotientUnit() {
+        Map<String, Integer> divisorFormula = new HashMap<>();
+        divisorFormula.put("a", 1);
+        divisorFormula.put("e", -2);
+        UnitOfMeasure divisor = new UnitOfMeasure(divisorFormula);
+        UnitOfMeasure result = initialUnit.divide(divisor);
+        Map<String, Integer> expectedResultFormula = new HashMap<>();
+        expectedResultFormula.put("c", -2);
+        expectedResultFormula.put("b", 3);
+        expectedResultFormula.put("e", 2);
+        UnitOfMeasure expectedResult = new UnitOfMeasure(expectedResultFormula);
+        assertTrue(result.match(expectedResult));
+    }
+
+}
