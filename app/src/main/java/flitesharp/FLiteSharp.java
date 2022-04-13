b929f6cb4f506e039b3d88c7a505742ee97d1a00
SimoneMezzaro <simone.mezzaro@mail.polimi.it>
2022-04-13 17:17:24 +0800

    Update how FLiteSharp class manages parser and IO exceptions

--- app/src/main/java/flitesharp/FLiteSharp.java
+++ app/src/main/java/flitesharp/FLiteSharp.java
@@ -4,7 +4,6 @@ import flitesharp.component.Component;
 
 import flitesharp.component.environment.EnvFrame;
 import flitesharp.exception.CompilingException;
-import flitesharp.type.TypeElement;
 import io.antlr.gen.FLiteSharpLexer;
 import io.antlr.gen.FLiteSharpParser;
 import io.antlr.gen.FLiteSharpVisitor;
@@ -12,74 +11,55 @@ import io.antlr.gen.FLiteSharpVisitor;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.misc.ParseCancellationException;
 import org.antlr.v4.runtime.tree.ParseTree;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Objects;
 
 public class FLiteSharp {
 
     private static Component processAntlr(CharStream inputStream) {
-        ThrowingErrorListener errorListener = ThrowingErrorListener.getInstance();
         FLiteSharpLexer lexer = new FLiteSharpLexer(inputStream);
-        lexer.removeErrorListeners();
-        lexer.addErrorListener(errorListener);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         FLiteSharpParser parser = new FLiteSharpParser(tokens);
-        parser.removeErrorListeners();
-        parser.addErrorListener(errorListener);
         ParseTree tree = parser.start();
+        if(parser.getNumberOfSyntaxErrors() > 0) {
+            return null;
+        }
         FLiteSharpVisitor<Component> visitor = new FLiteSharpComponentsCreatorVisitor();
-
         return visitor.visit(tree);
     }
 
     private static Component processIO(String fileName) {
-        CharStream inputStream = null;
+        CharStream inputStream;
         try {
             inputStream = CharStreams.fromStream(
                     Objects.requireNonNull(FLiteSharp.class.getResourceAsStream("/" + fileName)));
-            //inputStream = CharStreams.fromFileName(FILE_PATH);
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException | NullPointerException e) {
+            System.err.println("Unable to read input file");
+            return null;
         }
-
         return processAntlr(inputStream);
     }
 
     private static void evaluate(Component root) {
-
         EnvFrame initEnv = new EnvFrame();
-
-        // System.out.println("Parsed string:\n" + root.getStringRepresentation());
         System.out.println("Evaluate Result:\n" + root.evaluate(initEnv).getStringRepresentation());
     }
 
     private static void typeCheck(Component root) throws CompilingException {
-
-        EnvFrame initEnv = new EnvFrame(null, new HashMap<>());
-
-        // System.out.println("Parsed string:\n" + root.getStringRepresentation());
-        TypeElement type = root.checkType(initEnv);
-        // System.out.println("Type Check Result:\n" + type.getStringRepresentation());
+        EnvFrame initEnv = new EnvFrame();
+        root.checkType(initEnv);
     }
 
     public static void main(String[] args) {
-        Component root;
-        try {
-            root = processIO("type.txt");
-        }
-        catch (ParseCancellationException e) {
-            System.out.println(e.getMessage());
+        Component root = processIO("type.txt");
+        if(root == null)
             return;
-        }
         try {
             typeCheck(root);
-        }
-        catch (CompilingException e) {
-            System.out.println(e.getMessage());
+        } catch (CompilingException e) {
+            System.err.println(e.getMessage());
             return;
         }
         evaluate(root);
